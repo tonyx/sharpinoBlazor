b9b18507a129781fcb36ce85814908f0855f365e
Tonino Lucca <tonyx1@gmail.com>
2025-06-04 22:15:29 +0200

    env

--- SharpinoRecordStoreWebTests/Program.fs
+++ SharpinoRecordStoreWebTests/Program.fs
@@ -1,16 +1,20 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 module SharpinoRecordStore.WebTests
+open System
+open DotNetEnv
 open Expecto
 open Npgsql.FSharp
 open Npgsql
 open FSharpPlus
 
+Env.Load() |> ignore
+let password = Environment.GetEnvironmentVariable("password")
 let cleanDb () =
     let connection =
         "Server=127.0.0.1;"+
         "Database=sharpino_sample_auth_test;" +
         "User Id=postgres;"+
-        "Password=postgres;"
+        "Password={password};"
     connection
         |> Sql.connect
         |> Sql.query (sprintf "DELETE from public.\"AspNetUsers\"")
